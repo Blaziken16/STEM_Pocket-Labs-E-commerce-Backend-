919004aa0f984ecce1e2a1b7072d81785051a804
Blaziken16 <riashbhupadhyay16123@gmail.com>
2026-05-26 21:41:36 +0530

    chore: fixed the file structre & installed CORS plugin

--- src/main/kotlin/pocketLabs/plugins/Routing.kt
+++ src/main/kotlin/pocketLabs/plugins/Routing.kt
@@ -1,10 +1,14 @@
-package com.example
+package com.example.pocketLabs.plugins
 
 import io.ktor.server.application.*
+import io.ktor.server.plugins.cors.routing.CORS
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+    install(CORS){
+        anyHost()
+    }
     routing {
         get("/") {
             call.respondText("Hello, World!")
